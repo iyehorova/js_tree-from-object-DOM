67935d2db34fc407e9b0c17ea71e0c37a46c253f
Iryna Yehorova <yehorova.dev@gmail.com>
2024-07-28 17:16:34 +0300

    make build

---
+++ index.4422b298.js
@@ -0,0 +1,2 @@
+!function e(n,t){var r=Object.keys(t);if(0!==r.length){var a=document.createElement("ul");r.forEach(function(n){var r=document.createElement("li");r.textContent=n,e(r,t[n]),a.appendChild(r)}),n.appendChild(a)}}(document.querySelector("#tree"),{Drink:{Wine:{},Schnaps:{}},Fruit:{Red:{Cherry:{},Strawberry:{}},Yellow:{Banana:{},Pineapple:{}}}});
+//# sourceMappingURL=index.4422b298.js.map
